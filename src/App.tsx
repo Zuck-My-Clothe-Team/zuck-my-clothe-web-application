f082b7373a17db37c4013b29156509aa0f3b8115
PANURUT SIRINAPAISAN <panurut.siri@kmutt.ac.th>
2024-09-27 21:41:04 +0700

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { DatePicker } from "antd";
+
+function App() {
+  return (
+    <div className="h-screen flex">
+      <div className="m-auto">
+        <h4 className="text-bold text-2xl">Hello</h4>
+        <DatePicker />
+      </div>
+    </div>
+  );
+}
+
+export default App;
