a0b891a0c640e37a5e79d0f95aadc6bd21a5edb9
PANURUT SIRINAPAISAN <panurut.siri@kmutt.ac.th>
2024-12-05 22:30:53 +0700

    fix login screen

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -8,16 +8,27 @@ const LoginPage = () => {
   if (isLoading) return <LoadingPage />;
 
   return (
-    <div className="h-screen flex">
-      <div className="m-auto w-[60%]">
-        <div className="relative">
-          <img src="/images/loginFormBG.png" className="w-full z-[0]" />
-          <div className="absolute flex flex-col justify-center items-center inset-0 w-full gap-16">
-            <img src="/images/logo-withname.png" className="h-48" />
-            <div className="w-[60%]">
-              <LoginForm isLoading={isLoading} setIsLoading={setIsLoading} />
-            </div>
-          </div>
+    <div className="h-screen flex justify-center items-center relative">
+      <div className="absolute flex justify-center items-center z-[-1] w-full h-full">
+        <div
+          style={{
+            backgroundImage: "url('/images/loginFormBG.png')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            maxWidth: "60rem",
+            width: "100%",
+            height: "100%",
+          }}
+        ></div>
+      </div>
+      <div className="flex flex-col justify-center items-center w-3/4 lg:w-1/2 xl:w-1/3">
+        <img
+          src="/images/logo-withname.png"
+          className="h-28 lg:h-52"
+          loading="lazy"
+        />
+        <div className="w-full max-w-[1280px]">
+          <LoginForm isLoading={isLoading} setIsLoading={setIsLoading} />
         </div>
       </div>
     </div>
